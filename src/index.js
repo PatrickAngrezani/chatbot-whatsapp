62d311aa5a827cbc86c53b5e28429ce97996d17d
PatrickAngrezani <pangrezani@gmail.com>
2024-05-11 16:35:59 -0300

    feat: configure automatic receive and answer message

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
-const { Configuration, OpenAIPI } = require("openai");
-const fs = require("fs");
 require("dotenv").config;
 
-const atendimentoMapFile = "./atendimentoMap.json";
 const now = new Date();
 const hour = now.getHours();
 let daytime;
@@ -42,22 +39,19 @@ switch (hour) {
     break;
 }
 
-const saudacoes = ["oi"];
+const saudacoes = [
+  "oi",
+  "ola",
+  "olá",
+  "bom dia",
+  "boa tarde",
+  "boa noite",
+  "bom dia, tudo bem?",
+  "boa tarde, tudo bem?",
+  "boa noite, tudo bem?",
+];
 
 const options = ["1", "2", "3", "4"];
-let atendimentoMap = {};
-
-try {
-  const data = fs.readFileSync(atendimentoMapFile, "utf8");
-  atendimentoMap = JSON.parse(data);
-} catch (error) {
-  if (error.code === "ENOENT") {
-    console.log("AtendimentoMap file not found, creating a new one.");
-    atendimentoMap = {};
-  } else {
-    console.error("Error reading atendimentoMap file:", error);
-  }
-}
 
 const client = new Client({
   puppeteer: {
@@ -79,58 +73,13 @@ client.on("ready", () => {
   console.log("Connected");
 });
 
-client.on("message", async (option) => {
-  const clientOption = option.body.toLowerCase();
-
-  if (options.includes(clientOption)) {
-    showOptions(clientOption).then((result) => option.reply(result));
-  }
-
-  await saveAtendimentoMap();
-});
-
-//new model to configure historic talks
 client.on("message", async (msg) => {
-  const clientId = msg.from;
   const clientMessage = msg.body.toLowerCase();
 
-  //verify previous service "today"
-  const atendimento = atendimentoMap[clientId];
-
-  //configure message test
   if (saudacoes.includes(clientMessage)) {
-    if (
-      atendimento &&
-      atendimento.data === new Date().toISOString().slice(0, 10)
-    ) {
-      //service in same day, recover talk and continue
-      const conversa = atendimento.conversa;
-
-      //process new message and update talk
-      conversa.push(clientMessage);
-      atendimentoMap[clientId].conversa = conversa;
-
-      //send answer to user
-      await welcomeMessage(false).then((result) => msg.reply(result));
-
-      //save updated talk historic
-      await saveAtendimentoMap();
-    } else {
-      //save new service to customer
-      const novoAtendimento = {
-        data: new Date().toISOString().slice(0, 10),
-        conversa: [clientMessage],
-      };
-
-      //add new service to map
-      atendimentoMap[clientId] = novoAtendimento;
-
-      //send welcome message showing options
-      await welcomeMessage(true).then((result) => msg.reply(result));
-
-      //save service in localStorage
-      await saveAtendimentoMap();
-    }
+    await welcomeMessage(true).then((result) => msg.reply(result));
+  } else if (options.includes(clientMessage)) {
+    showOptions(clientMessage).then((result) => msg.reply(result));
   }
 });
 
@@ -187,15 +136,6 @@ async function welcomeMessage(firstTime) {
 2 - Dúvidas sobre o acesso ao player;
 3 - Configurações técnicas;
 4 - Outros setores.`;
-}
-
-async function saveAtendimentoMap() {
-  try {
-    const data = JSON.stringify(atendimentoMap);
-    fs.writeFileSync(atendimentoMapFile, data, "utf-8");
-  } catch (error) {
-    console.error("Error saving atendimentoMapFile:", error);
-  }
 }
 
 client.initialize();
