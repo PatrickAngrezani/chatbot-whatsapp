f7317f94babf3a79958da69f4b388a27009757e3
PatrickAngrezani <pangrezani@gmail.com>
2024-05-23 15:17:08 -0300

    feat: update mainly menu

--- src/options/comercial/menu-options.js
+++ src/options/comercial/menu-options.js
@@ -2,10 +2,15 @@ const menuOptions = `11 - Menu Principal:
 
 Por favor, selecione a opção a seguir para seguir com o atendimento:
 1 - Serviços e Produtos;
-2 - Orçamento personalizado;
+2 - Planos e Valores;;
 3 - Parcerias e Publicidade;
-4 - Agendamento de Reuniões;
-5 - Outros setores;
-6 - Voltar ao Menu Principal.`;
+4 - Processo de contratação;
+5 - Acervo Royalty Free;
+6 - Spots;
+7 - Legislação & Direitos Autorais;
+8 - Métricas;
+9 - Fale com o setor Comercial;
+10 - Outros setores;
+11 - Voltar ao Menu Principal.`;
 
 module.exports = menuOptions;
