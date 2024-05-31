8e03f99d253fb62ca28ea76180d8fba08963bfa4
PatrickAngrezani <pangrezani@gmail.com>
2024-05-31 01:26:11 -0300

    feat: update option 5 suporte

--- src/options/suporte/sectors-option.js
+++ src/options/suporte/sectors-option.js
@@ -1,10 +1,15 @@
 const sectorsOption = `5 - Outros setores:
 
 Selecione o setor de sua preferência:
-- Comercial;
-- Customer Success;
-- Financeiro;
-- Suporte - Raphael Melo;
-- Suporte - Valdiene Goes.`;
+- Customer Success 
+        - (51) 98063-1413;
+        - (51) 98544-0509
+- Comercial 
+        - (11) 94270-0889
+- Financeiro
+        - (51) 98546-8899
+- Suporte Técnico
+        - (51) 99609-5602;
+        - (51) 99669-5926`;
 
 module.exports = sectorsOption;
