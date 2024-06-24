8918cc3ca84deb35b2bbd803270bfe340f0d729b
PatrickAngrezani <patricksantos99@outlook.com>
2024-06-23 22:06:33 -0300

    feat: update customer-success bot

--- src/sectors/customer-success.js
+++ src/sectors/customer-success.js
@@ -16,14 +16,13 @@ const customerSuccess = require("../options/customer-success/customer-success-se
 const cancelContract = require("../options/customer-success/cancel-contract");
 const menuOptions = require("../options/customer-success/menu-options");
 const customerSuccessMenu = require("../options/menu/customer-success-menu");
-const saudacoes = require("../saudations/saudations");
 const cancelHits = require("../options/general/cancel-contract/cancel-hits");
 const cancelRoyaltyFree = require("../options/general/cancel-contract/cancel-royalty-free");
 
 require("dotenv").config;
 
 const options = ["1", "2", "3", "4", "5", "6", "7", "8"];
-const generalFunctions = require("../sectors/general/general-functions");
+const generalFunctions = require("./general/general");
 
 const client = new Client({
   puppeteer: {
@@ -52,30 +51,19 @@ client.on("message", async (msg) => {
   const msgFrom = msg.from;
   const msgAuthor = msg.author;
   const isGroupMessage = msgFrom.includes("g");
-  const companyNumbers = [
-    "5511942700889@c.us",
-    "5511975983317@c.us",
-    "555186116422@c.us",
-    "553898548432@c.us",
-    "555196095602@c.us",
-    "555196695926@c.us",
-    "555185468899@c.us",
-    "555198763990@c.us",
-    "555180631413@c.us",
-    "555186070833@c.us",
-    "555185440509@c.us",
-    "555184648888@c.us",
-    "555180326030@c.us",
-    "5518996074748@c.us",
-  ];
+  const numberOfWords = clientMessage.split(" ").length;
 
   if (dateMsg >= timeStarted) {
-    if (!companyNumbers.includes(msgFrom)) {
+    if (!generalFunctions.companyNumbers.includes(msgFrom)) {
       if (!isGroupMessage) {
         const hasService = await generalFunctions.hasService(
           msgFrom.split("@")[0]
         );
-        if (saudacoes.includes(clientMessage)) {
+        const hasGreetings = await generalFunctions.checkGreetings(
+          clientMessage
+        );
+
+        if (hasGreetings && numberOfWords <= 6) {
           await welcomeMessage(hasService).then((result) => msg.reply(result));
           await showMenu(clientMessage).then((result) => msg.reply(result));
         } else if (options.includes(clientMessage)) {
