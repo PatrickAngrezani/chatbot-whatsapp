e37a8e58d28bee117aa10487bc8034bd0781fa48
PatrickAngrezani <pangrezani@gmail.com>
2024-08-06 14:53:23 -0300

    feat: update bot sending message

--- src/sectors/comercial.js
+++ src/sectors/comercial.js
@@ -193,10 +193,12 @@ client.on("message", async (msg) => {
 
           if (hasGreetings && numberOfWords <= 6) {
             await welcomeMessage(hasService).then((result) =>
-              msg.reply(result)
+              client.sendMessage(`${msgFrom}`, result)
             );
           } else if (options.includes(clientMessage)) {
-            showOptions(clientMessage).then((result) => msg.reply(result));
+            showOptions(clientMessage).then((result) =>
+              client.sendMessage(`${msgFrom}`, result)
+            );
           }
           await generalFunctions.saveService(
             msgFrom.split("@")[0],
