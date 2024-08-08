709f3ce216baa3c2d9b3929927f8067ce4c58591
PatrickAngrezani <pangrezani@gmail.com>
2024-08-08 14:01:05 -0300

    feat: configure pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,10 @@ module.exports = {
   apps: [
     {
       name: "chatbot-infymedia",
-      script: "./src/sectors/comercial.js",
+      script: "pm2 start ./src/sectors/comercial.js --name chatbot-infymedia",
       instances: 1,
       autorestart: true,
-      watch: false,
+      watch: true,
       max_memory_restart: "1G",
       env: {
         NODE_ENV: "production",
