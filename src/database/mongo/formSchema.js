017f9eeeadccc587cebc19d63f3ac582a7976e55
PatrickAngrezani <pangrezani@gmail.com>
2024-09-09 15:56:38 -0300

    feat: create form schema

---
+++ src/database/mongo/formSchema.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const formSchema = new mongoose.Schema({
+  client_phone: Number,
+  question: String,
+  answer: String,
+  date: Date,
+});
+
+const Form = mongoose.model("Form", formSchema);
+
+module.exports = { Form };
