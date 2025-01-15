e44277db281ff7935692353859fbea318ca972a0
PatrickAngrezani <pangrezani@gmail.com>
2025-01-15 11:30:37 -0300

    feat: update values of plans

--- src/options/comercial/plans-and-values.js
+++ src/options/comercial/plans-and-values.js
@@ -2,20 +2,27 @@ const plansAndValues = `2 - Planos e Valores
 
 Em ambos os planos disponibilizamos a gestão de Rádio Indoor, de forma inteligente e interativa.
 
-Com o cupom de desconto: *ESCUTE20*
+- Plano Hits - Para até 200m ², R$ 99,90 mensais
+- Plano Royalty FREE- Para até 200m² R$ 169,00 mensais
 
-Pacote Hits R$ 79,92 mensais COM a inclusão de spots.
-Esse pacote são músicas de artistas consagrados, mas não isenta o pagamento de ECAD.
+- Plano Hits  Para até 499m², R$ 124,37 mensais
+- Plano Royalty FREE - Para até 499m², R$ 217,65 mensais
 
-Pacote Royalty Free R$ 135.20 mensais COM a inclusão de spots.
-Esse pacote isenta o pagamento de ECAD, contendo faixas de diversos gêneros licenciadas pelo artista.
+- Para até 2499m² o valor do nosso Plano Hits  R$ 148,83 mensais
+- Para até 2499m² o valor do nosso Plano Royalty FREE  R$ 266,30 mensais
+
+Descontos exclusivos para parceiros Gympass/Wellhub   Com o cupom de desconto: ESCUTE20 
+- Plano Hits fica:  R$ 79,92 mensais
+- Plano Royalty FREE fica: R$ 135,20 mensais
+
+- Hits: Esse pacote são músicas de artistas consagrados, mas não isenta o pagamento de ECAD.
+- Royalty Free: Esse pacote isenta o pagamento de ECAD, contendo faixas de diversos gêneros licenciadas pelo artista.
 
 Somos a única empresa no Brasil que faz de acordo com a Lei do Licenciamento Individual, no qual o ECAD não pode cobrar. 
 
 Ao fecharem esse plano conosco, nós mesmos informamos ao ECAD sobre a dispensa e em até 48h a isenção passa a valer! 
 
 Damos garantia jurídica na isenção, desde que seja utilizado esse acervo.
-
 
 Qual desses 2 planos, você tem interesse? 🤩`;
 
